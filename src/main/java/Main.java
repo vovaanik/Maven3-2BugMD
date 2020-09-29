39c4fb95ef53d2b4c535e0da6f62ae6a428572ea
vovaanik <vovaanik@yandex.ru>
2020-09-29 14:43:06 -0500

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,9 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    long bonus = service.calculate(1000_60, true);
+    System.out.println(bonus);
+
   }
 }
 
